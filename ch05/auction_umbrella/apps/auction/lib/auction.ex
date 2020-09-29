eaadafa765a78dd9a02bdaf1dc10d7cdf697e63d
Robin Malhotra <me@rmalhotra.com>
2020-09-30 00:52:18 +0100

    Split into products
    Signed-off-by: Robin Malhotra <me@rmalhotra.com>

--- ch05/auction_umbrella/apps/auction/lib/auction.ex
+++ ch05/auction_umbrella/apps/auction/lib/auction.ex
@@ -1,18 +1,18 @@
+
 defmodule Auction do
-  @moduledoc """
-  Documentation for `Auction`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Auction.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  alias Auction.{FakeRepo, Item}
+  
+  @repo FakeRepo
+  
+  def list_items() do
+    @repo.all(Item)
+  end
+  
+  def get_item(id) do
+    @repo.get!(Item, id)
+  end
+  
+  def get_item_by(attrs) do
+    @repo.get_by(Item, attrs)
   end
 end
