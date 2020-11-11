686010e9f5935dfbc700f62e3d5568b7991c4c8a
Robin Malhotra <me@rmalhotra.com>
2020-11-11 01:20:13 +0000

    Changesets!
    Signed-off-by: Robin Malhotra <me@rmalhotra.com>

--- ch05/auction_umbrella/apps/auction/lib/auction/item.ex
+++ ch05/auction_umbrella/apps/auction/lib/auction/item.ex
@@ -1,10 +1,19 @@
 defmodule Auction.Item do
   use Ecto.Schema
 
+  @changeset_validating_fields [:title, :description, :ends_at]
+
   schema "items" do
     field(:title, :string)
     field(:description, :string)
     field(:ends_at, :utc_datetime)
     timestamps()
+  end
+
+  def changeset(item, params \\ %{}) do
+    item
+    |> Ecto.Changeset.cast(params, @changeset_validating_fields)
+    |> Ecto.Changeset.validate_required(:title)
+    |> Ecto.Changeset.validate_lenght(:title, min: 3)
   end
 end
