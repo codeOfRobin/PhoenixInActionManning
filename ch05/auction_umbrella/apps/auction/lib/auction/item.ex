ce45b16ffb41cb0a0111f77a083973310810e1c9
Robin Malhotra <me@rmalhotra.com>
2020-11-14 00:14:57 +0000

    Starting chapter 9 after this
    Signed-off-by: Robin Malhotra <me@rmalhotra.com>

--- ch05/auction_umbrella/apps/auction/lib/auction/item.ex
+++ ch05/auction_umbrella/apps/auction/lib/auction/item.ex
@@ -1,5 +1,6 @@
 defmodule Auction.Item do
   use Ecto.Schema
+  import Ecto.Changeset
 
   @changeset_validating_fields [:title, :description, :ends_at]
 
@@ -12,8 +13,17 @@ defmodule Auction.Item do
 
   def changeset(item, params \\ %{}) do
     item
-    |> Ecto.Changeset.cast(params, @changeset_validating_fields)
-    |> Ecto.Changeset.validate_required(:title)
-    |> Ecto.Changeset.validate_lenght(:title, min: 3)
+    |> cast(params, @changeset_validating_fields)
+    |> validate_required(:title)
+    |> validate_length(:title, min: 3)
+    |> validate_length(:description, max: 200)
+    |> validate_change(:ends_at, &validate/2)
+  end
+
+  defp validate(:ends_at, ends_at_date) do
+    case Date.compare(ends_at_date, Date.utc_now()) do
+      :lt -> [ends_at: "can't be in the past"]
+      _ -> []
+    end
   end
 end
