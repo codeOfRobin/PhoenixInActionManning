06930f081934d935d9a8ef9e25662a27bcc0290b
Robin Malhotra <me@rmalhotra.com>
2020-11-21 00:25:01 +0000

    Add stuff for :new
    Signed-off-by: Robin Malhotra <me@rmalhotra.com>

--- ch05/auction_umbrella/apps/auction/lib/auction.ex
+++ ch05/auction_umbrella/apps/auction/lib/auction.ex
@@ -1,26 +1,27 @@
-
 defmodule Auction do
-  alias Auction.Item
-  
+  alias Auction.{Item, Repo}
+
   @repo Auction.Repo
-  
+
   def list_items() do
     @repo.all(Item)
   end
-  
+
   def get_item(id) do
     @repo.get!(Item, id)
   end
-  
+
   def get_item_by(attrs) do
     @repo.get_by(Item, attrs)
   end
-  
-  def insert_item(attrs) do 
+
+  def insert_item(attrs) do
     Auction.Item
     |> struct(attrs)
     |> @repo.insert()
   end
-  
+
+  def new_item(), do: Item.changeset(%Item{})
+
   def delete_item(%Auction.Item{} = item), do: @repo.delete(item)
 end
