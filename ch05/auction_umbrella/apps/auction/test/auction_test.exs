72917db23c1a4ebf208e3fae8951e6c38d501f7b
Robin Malhotra <malhotra.robin@yahoo.co.in>
2021-02-18 00:52:09 +0000

    add docs

--- ch05/auction_umbrella/apps/auction/test/auction_test.exs
+++ ch05/auction_umbrella/apps/auction/test/auction_test.exs
@@ -39,6 +39,22 @@ defmodule AuctionTest do
   end
 
   describe "insert_item/1" do
-    
+    test "adds an item to the database" do
+      count_query = from(i in Item, select: count(i.id))
+      before_count = Repo.one(count_query)
+      {:ok, _item} = Auction.insert_item(%{title: "test item"})
+      assert Repo.one(count_query) == before_count + 1
+    end
+
+    test "the Item in the database has the attributes provided" do
+      attrs = %{title: "test item", description: "test description"}
+      {:ok, item} = Auction.insert_item(attrs)
+      assert item.title == attrs.title
+      assert item.description == attrs.description
+    end
+
+    test "it returns an error on error" do
+      assert {:error, _changeset} = Auction.insert_item(%{foo: :bar})
+    end
   end
 end
--- ch05/auction_umbrella/mix.exs
+++ ch05/auction_umbrella/mix.exs
@@ -6,7 +6,8 @@ defmodule AuctionUmbrella.MixProject do
       apps_path: "apps",
       version: "0.1.0",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "Auction Umbrella"
     ]
   end
 
@@ -17,6 +18,7 @@ defmodule AuctionUmbrella.MixProject do
   # Run "mix help deps" for examples and options.
   defp deps do
     [
+      {:ex_doc, "~> 0.19", dev: true, runtime: false}
     ]
   end
 end
