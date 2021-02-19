988d5eb18789805e496c36ce312d3efac1a02854
Robin Malhotra <malhotra.robin@yahoo.co.in>
2021-02-19 02:04:12 +0000

    Add module docs

--- ch05/auction_umbrella/apps/auction/lib/auction.ex
+++ ch05/auction_umbrella/apps/auction/lib/auction.ex
@@ -1,4 +1,16 @@
 defmodule Auction do
+  @moduledoc """
+  Provides functions for interacting with the database layer of an Auction
+     application.
+  In order to keep database concerns separate from the rest of an
+     application, these
+  functions are provided. Any interaction you need to do with the database
+     can be done
+  from within these functions. See an individual function’s documentation for more
+  information and usage examples (like
+     `Auction.get_user_by_username_and_password/2`).
+  """
+
   alias Auction.{Bid, Item, Repo, User, Password}
   import Ecto.Query
 
@@ -65,6 +77,16 @@ defmodule Auction do
   You can then use the returned value to determine whether or not the User is
   authorized in your application. If an `Auction.User` is _not_ found based on
   `username`, the computational work of hashing a password is still done.
+
+  ## Examples
+
+    iex> Auction.insert_user(%{username: "geo", password: "example", password_confirmation: "example", email_address: "test@example.com"})
+    ...> result = Auction.get_user_by_username_and_password("geo", "example")
+    ...> match?(%Auction.User{username: "geo"}, result)
+    true
+
+    iex> Auction.get_user_by_username_and_password("no_user", "bad_password")
+    false
   """
   def get_user_by_username_and_password(username, password) do
     with user when not is_nil(user) <- @repo.get_by(User, %{username: username}),
